5507ffb735aa6ac412176972ded44914e4c1cba4
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-12-03 17:19:37 -0800

    names

---
+++ student.h
@@ -0,0 +1,20 @@
+#include <string> 
+#ifndef _STUDENT_H_
+#define _STUDENT_H
+
+
+class student
+{
+
+private:
+        std::string first;
+        std::string last;
+
+public:
+        void setName(std::string firstName, std::string lastName);
+        std::string fullName();
+
+        
+};
+
+#endif
